1b19214d0f9610780c5876c74410a9007c2ffee6
lam4ik611 <lam4ik611@gmail.com>
2020-08-20 01:02:02 +0300

    added section scroll

--- assets/js/animation/animation.js
+++ assets/js/animation/animation.js
@@ -9,21 +9,26 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
 gsap.registerPlugin(Draggable);
 gsap.registerPlugin(TweenMax);
+gsap.registerPlugin(TimelineMax);
 
 class Animation {
     constructor() {
+        this.animatedWalkingContainer = '[data-el="animated.container"]';
         this.animatedContainer = '[data-el="animated.container"]';
         this.animatedElement = '[data-el="animated.element"]';
         this.animatedPerson = '[data-el="animated.person"]';
         this.viewportHeight = window.innerHeight;
+
         this.elements = document.querySelectorAll(this.animatedElement);
         this.person = document.querySelector(this.animatedPerson);
+        this.wrapper = document.querySelector('[data-el="animated.wrapper"]');
+        this.sections = document.querySelectorAll('[data-el="animated.section"]');
+        this.preview = document.querySelectorAll('[data-el="animated.preview"]');
+
         this.isScrolling = null;
-        this.scrollHeight = document.querySelector(this.animatedContainer).offsetWidth;
+        this.container = document.querySelector(this.animatedContainer);
 
         this.scroller = {
-            ease: 0.0005, // <= scroll speed
-            endX: 0,
             x: 0,
             resizeRequest: 1,
             scrollRequest: 0,
@@ -33,7 +38,7 @@ class Animation {
 
     init() {
         TweenLite.set('body', {
-            height: this.scrollHeight,
+            height: this.container.offsetWidth,
             overflowY: 'scroll',
             //rotation: 0.01,
             //force3D: true
@@ -43,71 +48,95 @@ class Animation {
             //this.update();
             window.focus();
             //this.walking();
-            window.addEventListener('resize', () => this.onResize());
+            this.scrollTrigger();
             document.addEventListener('scroll', () => this.onScroll());
         });
-    }
-
-    onResize() {
-        this.scroller.resizeRequest++;
-        if (!this.requestId) {
-            this.requestId = requestAnimationFrame(() => this.update());
-        }
     }
 
     onScroll() {
         this.scroller.scrollRequest++;
         if (!this.requestId) {
-            this.requestId = requestAnimationFrame(() => this.update());
+            this.requestId = requestAnimationFrame(() => this.parallax());
         }
+    }
+
+    scrollTrigger() {
+        const sectionContainer = document.querySelector('[data-section]');
+        const globalContext = this;
+
+        this.sections.forEach((value, index) => {
+            ScrollTrigger.create({
+                trigger: value,
+                toggleActions: 'play pause reverse pause',
+                toggleClass: 'visible',
+                start: `${index * value.offsetWidth} center`,
+                end: `${(index + 1) * value.offsetWidth} center`,
+                scrub: true,
+                //pinSpacing: false
+            });
+        });
+
+        gsap.to(this.preview, {
+            scrollTrigger: {
+                trigger: this.preview,
+                start: `${window.innerWidth / 3} top`,
+                end: `${window.innerWidth} bottom`,
+                toggleActions: 'play reverse play reverse',
+            },
+            y: function (index, target, targets) {
+                return -target.offsetHeight - 20;
+            },
+            duration: 3,
+        });
     }
 
     walking(state) {
         const spriteSheet = {
-            total: 6,
+            frames: 5,
+            defaultFrame: 7,
             duration: 1,
         };
 
         const personTimeline = new TimelineMax();
-        for (let i = 0; i <= spriteSheet.total; i++) {
+
+        for (let i = 0; i <= spriteSheet.frames; i++) {
             personTimeline
                 .set(this.person, {
-                    onCompleted: i === spriteSheet.total ? () => this.walking(true) : {},
+                    ...i === spriteSheet.frames ? {
+                        onComplete: () => this.walking(true),
+                    } : {},
                     attr: {
                         ['data-step']: i,
-                    }
-                }, i / spriteSheet.total * spriteSheet.duration)
+                    },
+                }, i / spriteSheet.frames * spriteSheet.duration)
         }
 
         if (!state) {
             TweenMax.killTweensOf(this.person);
             setTimeout(() => {
-                this.person.dataset.step = 7;
+                this.person.dataset.step = spriteSheet.defaultFrame;
             }, 200);
         }
     }
 
-    update() {
+    parallax() {
         const scrollY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
         const resized = this.scroller.resizeRequest > 0;
 
-        this.scroller.endX = scrollY;
-        this.scroller.x += (scrollY - this.scroller.x) * this.scroller.ease;
+        this.scroller.x = scrollY;
 
         if (Math.abs(scrollY - this.scroller.x) < 0.05 || resized) {
-            this.scroller.x = scrollY;
             this.scroller.scrollRequest = 0;
         }
 
         gsap.to(this.elements, {
-            scrollTrigger: this.elements,
             x: (index, target) => {
-                return -(this.scroller.x / 2) * target.dataset.speed;
+                return -this.scroller.x * target.dataset.speed;
             },
             force3D: true,
         });
 
-        this.requestId = this.scroller.scrollRequest > 0 ? requestAnimationFrame(this.update) : null;
+        this.requestId = this.scroller.scrollRequest > 0 ? requestAnimationFrame(this.parallax) : null;
 
         // detect started/stopped scroll
         if (this.isScrolling === null) {
@@ -117,7 +146,6 @@ class Animation {
         }
 
         this.isScrolling = setTimeout(() => {
-            console.log( 'Scrolling has stopped.' );
             this.walking(false);
             this.isScrolling = null;
         }, 500);
