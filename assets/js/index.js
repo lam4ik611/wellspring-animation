931c6b2461cdfa4d3938dc1e37f7380c91530d04
lam4ik611 <lam4ik611@gmail.com>
2020-09-10 20:17:02 +0300

    fix house on mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,5 @@
 import '../scss/index.scss';
-import Swiper from "swiper";
 
 import './animation';
 import './preloader';
 import './is-support-webp';
-
-let swiper = new Swiper('#mySwiper', {
-    direction: 'vertical',
-    on: {
-        init: function () {
-            console.log('swiper initialized');
-        },
-    },
-});
