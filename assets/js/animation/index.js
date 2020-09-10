f8a4e7bffaa0e1b76e2402bcc70a5d909fc71069
lam4ik611 <lam4ik611@gmail.com>
2020-09-10 19:55:40 +0300

    swiper testing

--- assets/js/animation/index.js
+++ assets/js/animation/index.js
@@ -1,20 +1,8 @@
 import Animation from './animation';
 import MobileAnimation from './mobile-animation';
-import Swiper from 'swiper';
 
 if (window.innerWidth > 1024) { // TODO
     new Animation();
 } else {
     new MobileAnimation();
-
-    new Swiper('[data-el="animated.mobile-container"]', {
-        direction: 'vertical',
-        autoplay: 1000,
-        slidesPerView: 'auto',
-        speed: 200,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        }
-    });
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,23 @@
 import '../scss/index.scss';
+import Swiper from "swiper";
 
 import './animation';
 import './preloader';
 import './is-support-webp';
+
+let swiper = new Swiper('#mySwiper', {
+    direction: 'vertical',
+    on: {
+        init: function () {
+            console.log('swiper initialized');
+        },
+    },
+});
+
+swiper.on('slideChange', function () {
+    console.log('slide changed');
+});
+
+setTimeout(() => {
+    swiper.update();
+}, 1000);
